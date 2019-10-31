17ab2ae8f5c2f113ee88ad86288cc938d8bb4d67
NecJr <NecZhang@users.noreply.github.com>
2019-10-31 16:51:21 -0400

    Problem01Change

--- Prb01/Prb01.cpp
+++ Prb01/Prb01.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-	cout << "heihei";
+	string fName, lName;
+	char grade;
+	int age;
+	
+	cout << "What is your first name? ";
+	getline(cin, fName);
+	cout << "What is your last name? ";
+	getline(cin, lName);
+	cout << "What letter grade do you deserve? ";
+	cin >> grade;
+	cout << "What is your age? ";
+	cin >> age;
+
+	cout << "Name: " << lName << ", " << fName << "\nGrade: " << char(grade + 1) << "\nAge: " << age;
 
 	return 0;
 }
